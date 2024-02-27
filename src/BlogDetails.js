3231cab5cba5c27ed4030ba6904b9358d13a895c
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-02-27 10:44:18 +0630

    url fix

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -5,10 +5,10 @@ import useFetch from './useFetch';
 const BlogDetails = () => {
     const {id} = useParams();
     //http://localhost:8000/blogs/
-    const {data:blog, error, isPending} = useFetch('http://localhost:3000/blogs/'+id);
+    const {data:blog, error, isPending} = useFetch('https://json-server-vercel-main-eta.vercel.app/blogs/'+id);
     const history = useHistory();
     const handleClick = () => {
-        fetch('http://localhost:3000/blogs/'+blog.id,{
+        fetch('https://json-server-vercel-main-eta.vercel.app/blogs/'+blog.id,{
             method: 'DELETE',
         }).then(() => {
             history.push('/');
--- src/Create.js
+++ src/Create.js
@@ -12,7 +12,7 @@ const Create = () => {
         e.preventDefault();
         const blog = {title, body, author};
         setIsPending(true);
-        fetch('http://localhost:3000/blogs',{
+        fetch('https://json-server-vercel-main-eta.vercel.app/blogs',{
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(blog)
